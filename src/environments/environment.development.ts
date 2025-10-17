371c257bd9319e5d70cb2ec4747f32a0dc535e6e
Omar Eltantawy <110872105+omar01140@users.noreply.github.com>
2025-10-17 16:58:07 +0300

    supabase & tailwind integration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  supabaseUrl: 'https://ihyixtedbbfkklfoibwk.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloeWl4dGVkYmJma2tsZm9pYndrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2NTE4MDMsImV4cCI6MjA3NjIyNzgwM30.RW63jpCTDd_h3ViEc0q-w3vigCWoEPLD6oSgDzEV0jY',
+};
