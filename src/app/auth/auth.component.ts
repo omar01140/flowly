dec3324c441fe29e3c58bbb6ed0422f68372c4b5
Omar Eltantawy <110872105+omar01140@users.noreply.github.com>
2025-10-30 04:06:32 +0300

    Enhance form validation and UI feedback in auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,16 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
 export class AuthComponent {
-  isLogin = true;
+  isLogin = false;
   authService = inject(AuthService);
 
   onSwitch() {
@@ -26,6 +27,9 @@ export class AuthComponent {
     });
   }
   onSubmit(formData: NgForm) {
+    if (formData.invalid) {
+      return;
+    }
     const email = formData.form.value.email;
     const password = formData.form.value.password;
     const name = formData.form.value.name;
